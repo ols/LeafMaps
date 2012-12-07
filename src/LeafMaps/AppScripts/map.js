01422a839883708f2a48041ed7d3eba4cf6b70c6
ols <olov.sikstrom@gmail.com>
2012-12-07 20:41:56 +0100

    remove the zones

--- src/LeafMaps/AppScripts/map.js
+++ src/LeafMaps/AppScripts/map.js
@@ -5,20 +5,20 @@ LM.LoadMapWithZones = (function () {
 	var googleLayer = new L.Google('SATELLITE');
 	map.addLayer(googleLayer);
 
-	$.ajax({
-		type: "GET",
-		url: "zzz.xml",
-		dataType: "xml",
-		success: function (xml) {
-			$(xml).find('zone').each(function () {
-				var points = new Array();
-				$(this).find('point').each(function () {
-					var latitude = $(this).attr('lat');
-					var longitude = $(this).attr('long');
-					points.push(new L.LatLng(latitude, longitude));
-				});
-				L.polygon(points).addTo(map);
-			});
-		}
-	});
+//	$.ajax({
+//		type: "GET",
+//		url: "zzz.xml",
+//		dataType: "xml",
+//		success: function (xml) {
+//			$(xml).find('zone').each(function () {
+//				var points = new Array();
+//				$(this).find('point').each(function () {
+//					var latitude = $(this).attr('lat');
+//					var longitude = $(this).attr('long');
+//					points.push(new L.LatLng(latitude, longitude));
+//				});
+//				L.polygon(points).addTo(map);
+//			});
+//		}
+//	});
 })({});
