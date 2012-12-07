05bdc5201cc2d4799361453172a1cc093042c09a
ols <olov.sikstrom@gmail.com>
2012-12-07 16:42:28 +0100

    just a simple leafmap test with polygons

---
+++ src/LeafMaps/AppScripts/map.js
@@ -0,0 +1,24 @@
+﻿var LM = LM || {};
+
+LM.LoadMapWithZones = (function () {
+	var map = new L.Map('map', { center: new L.LatLng(59.319813, 17.986615), zoom: 9 });
+	var googleLayer = new L.Google('SATELLITE');
+	map.addLayer(googleLayer);
+
+	$.ajax({
+		type: "GET",
+		url: "zzz.xml",
+		dataType: "xml",
+		success: function (xml) {
+			$(xml).find('zone').each(function () {
+				var points = new Array();
+				$(this).find('point').each(function () {
+					var latitude = $(this).attr('lat');
+					var longitude = $(this).attr('long');
+					points.push(new L.LatLng(latitude, longitude));
+				});
+				L.polygon(points).addTo(map);
+			});
+		}
+	});
+})({});
